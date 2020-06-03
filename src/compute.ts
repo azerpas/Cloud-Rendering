19cd57ad051849dd745d67de486b480b39216915
azerpas <xskylix@hotmail.fr>
2020-06-03 15:10:10 +0200

    Create bucket and upload file to bucket functions

--- src/compute.ts
+++ src/compute.ts
@@ -1,18 +1,30 @@
 import {google} from 'googleapis'
 import "./lib/env"
 import { v4 as uuidv4 } from 'uuid';
+import { Storage } from '@google-cloud/storage'
+
+const getClient = async (scopes?: string[]) => {
+  // Getting client
+  try {
+    let authClient = null;
+    if(scopes === []){
+      authClient = await google.auth.getClient();
+    }else{
+      const auth = new google.auth.GoogleAuth({ scopes: scopes });
+      authClient = await auth.getClient()
+    }
+    return authClient;
+  } catch (error) {
+    console.error(error);
+    throw(error);
+  }
+}
 
 export const startInstance = async () => {
     const compute = google.compute("v1");
     console.log(`Starting instance: ${process.env.PROJECT_ID}/${process.env.ZONE}/${process.env.INSTANCE}`);
-    // Getting client
-    try {
-      const authClient = await google.auth.getClient();
-      google.options({auth: authClient});
-    } catch (error) {
-      console.error(error);
-      throw(error);
-    }
+    const authClient = await getClient();
+    google.options({auth: authClient});
     // Starting instance
     try {
       const requestId = uuidv4();
@@ -30,16 +42,31 @@ export const startInstance = async () => {
     }
 }
 
-export const uploadToDrive = async () => {
-  // Getting client
-  try {
-    const authClient = await new google.auth.OAuth2(
-      process.env.GDRIVE_CLIENT_ID, 
-      process.env.GDRIVE_CLIENT_SECRET, 
-      "urn:ietf:wg:oauth:2.0:oob"
-    );
-  } catch (error) {
-    console.error(error);
-    throw(error);
-  }
+export const uploadToInstance = async () => {
+  // https://cloud.google.com/compute/docs/instances/transfer-files#gcstransfer
+}
+
+export const createBucket = async () => {
+  const bucketName = "DAZ3D_"+uuidv4();
+  const storage = new Storage();
+  
+  const [bucket] = await storage.createBucket(bucketName, {
+    location: 'US-CENTRAL1'
+  });
+  
+  console.log(`Bucket ${bucket.name} created.`);
+  return bucketName;
+}
+
+export const uploadFileToBucket = async (bucketName: string, filepath: string) => {
+  const storage = new Storage();
+  await storage.bucket(bucketName).upload(filepath, {
+    // Support for HTTP requests made with `Accept-Encoding: gzip`
+    gzip: true,
+    // By setting the option `destination`, you can change the name of the
+    // object you are uploading to a bucket.
+  });
+
+  console.log(`${filepath} uploaded to ${bucketName}.`);
+  return true;
 }
